3715b719ef7b92285aa4dbeccd217f1fbecf867f
reshetnikov.f <reshetnikov.f@wb.ru>
2025-11-04 19:18:14 +0300

    Refactor FullscreenImage component to simplify styling by using 'inset' for positioning and add transition effects for smoother interactions.

--- src/components/MacrosTracker/FullscreenImage.tsx
+++ src/components/MacrosTracker/FullscreenImage.tsx
@@ -45,16 +45,13 @@ export function FullscreenImage({ src, style }: FullscreenImageProps) {
 		<Box
 			style={{
 				position: "fixed",
-				top: 0,
-				left: 0,
-				right: 0,
-				bottom: 0,
-				backgroundColor: "rgba(0, 0, 0, 0.95)",
+				inset: 0,
+				transition: "all 0.5s ease-in-out",
+				zIndex: 100,
+				cursor: "pointer",
 				display: "flex",
 				alignItems: "center",
 				justifyContent: "center",
-				zIndex: 100,
-				cursor: "pointer",
 			}}
 			onClick={() => startTransition(() => setIsOpen(false))}
 		>
