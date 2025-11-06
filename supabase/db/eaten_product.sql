bb4bc424a9c932b38c04f3b3ceba122031521ed6
reshetnikov.f <reshetnikov.f@wb.ru>
2025-11-06 23:57:51 +0300

    db migrations

--- supabase/db/eaten_product.sql
+++ supabase/db/eaten_product.sql
@@ -2,35 +2,39 @@
 CREATE TABLE public.eaten_product (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
   "createdAt" timestamp with time zone NOT NULL DEFAULT now(),
-  "productId" bigint NOT NULL,
-  value double precision NOT NULL,
+  protein bigint,
+  kcalories bigint,
+  unit text,
+  value double precision,
   date date DEFAULT now(),
-  "userId" uuid NOT NULL,
+  "userId" uuid DEFAULT gen_random_uuid(),
+  name text NOT NULL DEFAULT 'Продукт'::text,
+  "imageUrl" text,
   CONSTRAINT eaten_product_pkey PRIMARY KEY (id),
-  CONSTRAINT eaten_product_user_id_fkey FOREIGN KEY ("userId") REFERENCES auth.users(id) ON DELETE CASCADE,
-  CONSTRAINT eaten_product_product_id_fkey FOREIGN KEY ("productId") REFERENCES public.products(id) ON DELETE CASCADE
+  CONSTRAINT eaten_product_user_id_fkey FOREIGN KEY ("userId") REFERENCES auth.users(id)
 );
 
 -- Enable Row Level Security
 ALTER TABLE public.eaten_product ENABLE ROW LEVEL SECURITY;
 
 -- Create policies for eaten_product table
-CREATE POLICY "Users can view own eaten products"
+CREATE POLICY "Enable users to view their own data only"
   ON public.eaten_product
   FOR SELECT
+  TO authenticated
   USING (auth.uid() = "userId");
 
-CREATE POLICY "Users can insert own eaten products"
+CREATE POLICY "Enable insert for users based on user_id"
   ON public.eaten_product
   FOR INSERT
   WITH CHECK (auth.uid() = "userId");
 
-CREATE POLICY "Users can update own eaten products"
+CREATE POLICY "update"
   ON public.eaten_product
   FOR UPDATE
   USING (auth.uid() = "userId");
 
-CREATE POLICY "Users can delete own eaten products"
+CREATE POLICY "Enable delete for users based on user_id"
   ON public.eaten_product
   FOR DELETE
   USING (auth.uid() = "userId");
