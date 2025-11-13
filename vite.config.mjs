144b2d4d457c18c90ebe2ca3a41b8c72ae74b03b
reshetnikov.f <reshetnikov.f@wb.ru>
2025-11-13 21:37:19 +0300

    fix config

--- vite.config.mjs
+++ vite.config.mjs
@@ -23,37 +23,37 @@ export default defineConfig({
 				display: "standalone",
 				icons: [
 					{
-						src: "/android/android-launchericon-48-48.png",
+						src: "https://macros-count-app.fedorresh.ru/android/android-launchericon-48-48.png",
 						sizes: "48x48",
 						type: "image/png",
 						purpose: "any",
 					},
 					{
-						src: "/android/android-launchericon-72-72.png",
+						src: "https://macros-count-app.fedorresh.ru/android/android-launchericon-72-72.png",
 						sizes: "72x72",
 						type: "image/png",
 						purpose: "any",
 					},
 					{
-						src: "/android/android-launchericon-96-96.png",
+						src: "https://macros-count-app.fedorresh.ru/android/android-launchericon-96-96.png",
 						sizes: "96x96",
 						type: "image/png",
 						purpose: "any",
 					},
 					{
-						src: "/android/android-launchericon-144-144.png",
+						src: "https://macros-count-app.fedorresh.ru/android/android-launchericon-144-144.png",
 						sizes: "144x144",
 						type: "image/png",
 						purpose: "any",
 					},
 					{
-						src: "/android/android-launchericon-192-192.png",
+						src: "https://macros-count-app.fedorresh.ru/android/android-launchericon-192-192.png",
 						sizes: "192x192",
 						type: "image/png",
 						purpose: "any maskable",
 					},
 					{
-						src: "/android/android-launchericon-512-512.png",
+						src: "https://macros-count-app.fedorresh.ru/android/android-launchericon-512-512.png",
 						sizes: "512x512",
 						type: "image/png",
 						purpose: "any maskable",
